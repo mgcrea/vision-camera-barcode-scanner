939787b4f85a4b417c43d7ad0ce53f14d2430507
Olivier Louvignes <olivier@mgcrea.io>
2023-10-03 15:54:40 +0200

    chore(types): improve typings

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,10 @@
 import { NativeModules, NativeEventEmitter, Platform } from "react-native";
 import { VisionCameraProxy, type Frame } from "react-native-vision-camera";
-import type { FrameProcessorPlugin, VisionCameraConstants } from "./types";
+import type {
+  AndroidResponse,
+  FrameProcessorPlugin,
+  VisionCameraConstants,
+} from "./types";
 
 const LINKING_ERROR =
   `The package 'vision-camera-code-scanner' doesn't seem to be linked. Make sure: \n\n` +
@@ -31,7 +35,7 @@ export const visionCameraProcessorPlugin =
     MODULE_NAME,
   ) as FrameProcessorPlugin;
 
-export const scanBarcodes = (frame: Frame): unknown => {
+export const scanBarcodes = (frame: Frame): AndroidResponse => {
   "worklet";
-  return visionCameraProcessorPlugin.call(frame);
+  return visionCameraProcessorPlugin.call(frame) as unknown as AndroidResponse;
 };
--- src/types/module.ts
+++ src/types/module.ts
@@ -1,5 +1,23 @@
+import type { AndroidBarcode } from "./android";
+
 export type VisionCameraConstants = {
   MODULE_NAME: string;
   BARCODE_TYPES: { [key: string]: number };
   BARCODE_FORMATS: { [key: string]: number };
+};
+
+export type AndroidImage = {
+  orientation:
+    | "portrait"
+    | "landscape-right"
+    | "landscape-left"
+    | "portrait-upside-down";
+  pixelFormat: "yuv" | "rgb" | "native" | "unknown";
+  width: number;
+  height: number;
+};
+
+export type AndroidResponse = {
+  image: AndroidImage;
+  barcodes: AndroidBarcode[];
 };
