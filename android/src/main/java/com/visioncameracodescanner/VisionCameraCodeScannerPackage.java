6c8ff30025ee337472c120f93c8d4d7aed5d4ccc
Sean Coyle <scoyle@campusoptics.com>
2024-04-30 16:03:49 -0500

    Address changed paths on Android

--- android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPackage.java
+++ android/src/main/java/com/visioncameracodescanner/VisionCameraCodeScannerPackage.java
@@ -5,7 +5,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.mrousavy.camera.frameprocessor.FrameProcessorPluginRegistry;
+import com.mrousavy.camera.frameprocessors.FrameProcessorPluginRegistry;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
--- src/constants/templates.ts
+++ src/constants/templates.ts
@@ -4,15 +4,12 @@ export const Templates = {
   /**
    * HD-quality for faster Frame Processing in YUV pixelFormat (e.g. 720p)
    */
-  FrameProcessingYUV: [
-    { videoResolution: { width: 1080, height: 720 } },
-    { pixelFormat: "yuv" },
-  ],
+  FrameProcessingYUV: [{ videoResolution: { width: 1080, height: 720 } }, {}],
   /**
    * XGA-quality for faster Frame Processing in YUV pixelFormat
    */
   FrameProcessingBarcodeXGA: [
     { videoResolution: { width: 1024, height: 768 } },
-    { pixelFormat: "yuv" },
+    {},
   ],
 } as const satisfies Record<string, FormatFilter[]>;
